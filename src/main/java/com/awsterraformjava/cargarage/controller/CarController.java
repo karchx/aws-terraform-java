9957e1c122815ed7ca19ff06c60eedd4847d9774
karchx <kenethra.74@gmail.com>
2023-11-02 16:49:08 -0600

    feat: create car

--- src/main/java/com/awsterraformjava/cargarage/controller/CarController.java
+++ src/main/java/com/awsterraformjava/cargarage/controller/CarController.java
@@ -3,9 +3,10 @@ package com.awsterraformjava.cargarage.controller;
 import com.awsterraformjava.cargarage.repository.CarRepository;
 import com.awsterraformjava.cargarage.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +17,10 @@ public class CarController {
     @GetMapping("/cars")
     public List<Car> getAllCars() {
         return carRepository.findAll();
+    }
+
+    @PostMapping("/cars")
+    public Car createCar(@Valid @RequestBody Car car) {
+        return carRepository.save(car);
     }
 }
