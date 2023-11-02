b6e0609d27493cb677a5dcd0720512e0850f682a
karchx <kenethra.74@gmail.com>
2023-11-02 11:46:46 -0600

    chore: not connection db

--- src/main/java/com/awsterraformjava/cargarage/CargarageApplication.java
+++ src/main/java/com/awsterraformjava/cargarage/CargarageApplication.java
@@ -2,9 +2,19 @@ package com.awsterraformjava.cargarage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@RestController
 @SpringBootApplication
+@EnableJpaAuditing
 public class CargarageApplication {
+
+	@RequestMapping("/")
+    public String sayHello() {
+        return "Hello and Welcome to the CarGarage application.";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CargarageApplication.class, args);
---
+++ src/main/java/com/awsterraformjava/cargarage/model/Car.java
@@ -0,0 +1,39 @@
+package com.awsterraformjava.cargarage.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "cars")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updateAt" }, allowGetters = true)
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String model;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
