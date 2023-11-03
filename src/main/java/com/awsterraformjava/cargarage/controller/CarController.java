b40e7a5f10cdd08a948779eb390ad2925aa420f0
Стив <kenethra.74@gmail.com>
2023-11-02 21:12:20 -0600

    feat: controller update, delete and exception

--- src/main/java/com/awsterraformjava/cargarage/controller/CarController.java
+++ src/main/java/com/awsterraformjava/cargarage/controller/CarController.java
@@ -2,8 +2,10 @@ package com.awsterraformjava.cargarage.controller;
 
 import com.awsterraformjava.cargarage.repository.CarRepository;
 import com.awsterraformjava.cargarage.model.Car;
+import com.awsterraformjava.cargarage.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,8 +21,33 @@ public class CarController {
         return carRepository.findAll();
     }
 
+    @GetMapping("/cars/{id}")
+    public Car getCarById(@PathVariable(value = "id") Long carId) {
+      return carRepository.findById(carId)
+        .orElseThrow(() -> new ResourceNotFoundException("Car", "id", carId));
+    }
+
     @PostMapping("/cars")
     public Car createCar(@Valid @RequestBody Car car) {
         return carRepository.save(car);
+    }
+
+    @PutMapping("/cars/{id}")
+    public Car updateCar(@PathVariable(value = "id") Long carId, @Valid @RequestBody Car carDetail) {
+      Car car = carRepository.findById(carId).orElseThrow(() -> new ResourceNotFoundException("Car", "id", carId));
+      car.setMake(carDetail.getMake());
+      car.setModel(carDetail.getModel());
+
+      Car updatedCar = carRepository.save(car);
+
+      return updatedCar;
+    }
+
+    @DeleteMapping("/cars/{id}")
+    public ResponseEntity<?> deleteCar(@PathVariable(value = "id") Long carId) {
+      Car car = carRepository.findById(carId).orElseThrow(() -> new ResourceNotFoundException("Car", "id", carId));
+      carRepository.delete(car);
+
+      return ResponseEntity.ok().build();
     }
 }
